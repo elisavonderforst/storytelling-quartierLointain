88db78e4d3812a9fd0ae46e840f0d337167764ff
elisavonderforst <Elisa.vonderforst@studentfr.ch>
2024-11-12 17:14:45 +0100

    animation debut sans scoll

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,9 @@ const titleheader = document.querySelector("header");
 const titleImg = document.querySelector(".header-title");
 const butterfly = document.querySelector(".header-butterfly-img");
 
-var tl = gsap.timeline();
+var tl = gsap.timeline({
+  onComplete: () => (document.body.style.overflowY = "auto"),
+});
 tl.to(titleImg, { opacity: 1 });
 tl.to(titleImg, { duration: 3, opacity: 0 });
 tl.to(titleImg, { x: "100%", duration: 0.5, ease: "expo.out" });
@@ -19,3 +21,14 @@ tl.to(butterfly, { x: 300, y: -80 });
 tl.to(butterfly, { x: 500, y: -60, delay: 0 });
 tl.to(butterfly, { x: 600, y: 0, delay: 0 });
 tl.to(titleheader, { x: "100%", duration: 1, ease: "expo.out" });
+
+// when calculating pinning which involves sizing, make sure the scrollbar is factored in
+ScrollTrigger.addEventListener(
+  "refreshInit",
+  () => (document.body.style.overflowY = "auto")
+);
+ScrollTrigger.addEventListener("refresh", () => {
+  if (tl.progress() < 1) {
+    document.body.style.overflowY = "hidden";
+  }
+});
