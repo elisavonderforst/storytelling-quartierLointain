ca0383c41a3370d5ffcd186ee7c0959443302dd2
elisavonderforst <Elisa.vonderforst@studentfr.ch>
2024-11-14 11:11:12 +0100

    papillon

--- src/js/main.js
+++ src/js/main.js
@@ -5,22 +5,31 @@ const titleheader = document.querySelector("header");
 const titleImg = document.querySelector(".header-title");
 const butterfly = document.querySelector(".header-butterfly-img");
 
-var tl = gsap.timeline({
-  onComplete: () => (document.body.style.overflowY = "auto"),
+const tl = gsap.timeline({
+  onComplete: () => {
+    document.body.style.overflowY = "auto";
+    document.querySelector("main.is-hidden").classList.remove("is-hidden");
+  },
 });
 tl.to(titleImg, { opacity: 1 });
 tl.to(titleImg, { duration: 3, opacity: 0 });
 tl.to(titleImg, { x: "100%", duration: 0.5, ease: "expo.out" });
-tl.fromTo(
-  butterfly,
-  { x: "-100%", ease: "expo.out", delay: 0 },
-  { x: "100", ease: "expo.out", delay: 0 }
-);
-tl.to(butterfly, { x: 200, y: -20 });
-tl.to(butterfly, { x: 300, y: -80 });
-tl.to(butterfly, { x: 500, y: -60, delay: 0 });
-tl.to(butterfly, { x: 600, y: 0, delay: 0 });
-tl.to(titleheader, { x: "100%", duration: 1, ease: "expo.out" });
+tl.fromTo(butterfly, { x: "-100%", duration: 2 }, { x: "100", duration: 2 });
+tl.to(butterfly, { x: 200, y: -20, duration: 2 }, "-=1.7");
+tl.to(butterfly, { x: 300, y: -200, duration: 2 }, "-=1.7");
+tl.to(butterfly, { x: "80vh", y: -100, duration: 2 }, "-=1.7");
+tl.to(butterfly, { x: "80vh", y: 0, duration: 2 }, "-=1.7");
+tl.pause;
+butterfly.addEventListener("click", function () {
+  tl.resume();
+  tl.to(butterfly, { x: "200vh", y: -300 });
+  tl.to(titleheader, {
+    x: "100%",
+    opacity: 0,
+    duration: 0.3,
+    ease: "expo.out",
+  });
+});
 
 // when calculating pinning which involves sizing, make sure the scrollbar is factored in
 ScrollTrigger.addEventListener(
